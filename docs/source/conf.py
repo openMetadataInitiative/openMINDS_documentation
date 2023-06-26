c4faf85bbd999238f85b336e3dd8ab1a9e4a0ac5
Lyuba Zehl <l.zehl@fz-juelich.de>
2023-06-26 11:42:08 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'openMINDS'
+copyright = '2023, open Metadata Initiative'
+author = 'Zehl'
 
 release = '0.1'
 version = '0.1.0'
