c0a66399327f12b4268f83955b4711c280f4a353
Oliver Schmid <info@olischmid.ch>
2023-06-29 22:25:23 +0200

    reattach version selection

--- docs/_static/custom.js
+++ docs/_static/custom.js
@@ -3,4 +3,8 @@ window.onload = function() {
     let currentVersion = document.getElementsByClassName("rst-current-version")[0];
     currentVersion.innerHTML = currentVersion.innerHTML.replace(/\n/g, '').replace(/.*v: /m, '').trim();
     currentVersion.style.display =  "block";
+    let navig = document.getElementsByClassName("wy-side-nav-search")[0];
+    navig.appendChild(currentVersion);
+
+
   };
