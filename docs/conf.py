3c7d2dd82f90694384347f3a8f9e6435e6e0372e
Lyuba Zehl <l.zehl@fz-juelich.de>
2023-07-06 14:45:54 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -17,6 +17,8 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
+    'sphinxcontrib.osexample',
+    'sphinx.ext.autosectionlabel'
 ]
 
 intersphinx_mapping = {
