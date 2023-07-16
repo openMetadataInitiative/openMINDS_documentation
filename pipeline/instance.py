ce9aa85b9da70a49c19f06c8f1813ecf0c8b1769
lzehl <l.zehl@fz-juelich.de>
2023-07-16 19:45:33 +0200

    struggle with version links

--- pipeline/instance.py
+++ pipeline/instance.py
@@ -128,7 +128,7 @@ class InstancesDocBuilder(object):
         for name, vdict  in sorted(versions.items()):
             vdata = vdict["atlas"] if productType == "brainAtlases" else vdict
             vID = vdata['versionIdentifier']
-            space_html_title = f"{vdata['shortName'].replace(' ', '%20')}.html#{vID.replace(' ', '-')}"
+            space_html_title = f"{vdata['shortName'].replace(' ', '%20')}.html#version-{vID.replace(' ', '-')}"
             link = os.path.join(self.readthedocs_url, self.version, "libraries", productType, space_html_title)
             linklist.append(f"`{vID} <{link}>`_")
         return ", ".join(linklist)
@@ -288,11 +288,10 @@ class InstancesDocBuilder(object):
                 doc.newline()
                 doc.content("------------")
                 doc.newline()
-                doc.heading(f"Versions", char="#")
                 for _, vdict in sorted(data_to_display["versions"].items()):
                     vdata = vdict["atlas"]
                     subtitle = vdata['versionIdentifier']
-                    doc.heading(f"{subtitle}", char="*", overline=True)
+                    doc.heading(f"version {subtitle}", char="#")
                     doc.newline()
                     doc.directive(name="admonition", arg="metadata sheet")
                     doc.newline()
