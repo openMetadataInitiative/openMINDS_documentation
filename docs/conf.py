630b51655e0c2e3bf95af11cadcdf48164e6f413
Lyuba Zehl <l.zehl@fz-juelich.de>
2023-10-10 10:18:45 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,8 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx'
+    'sphinx.ext.intersphinx',
+    'sphinx_code_tabs'
 ]
 
 intersphinx_mapping = {
