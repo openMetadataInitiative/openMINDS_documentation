9e6ea91508c4d6215a74bbae2f0d61b5f3b12c12
Lyuba Zehl <lyuba.zehl@ebrains.eu>
2024-01-22 13:44:09 +0100

    Update schema.py
    fixing #13

--- pipeline/schema.py
+++ pipeline/schema.py
@@ -40,7 +40,7 @@ class SchemaDocBuilder(object):
             doc.newline()
             if "controlledTerms" in self._schema_payload["_type"]:
                 library_subdir = f"terminologies/{schema_name_camelCase}.html"
-                library_link = os.path.join(self.readthedocs_url, self.version, "libraries", library_subdir)
+                library_link = os.path.join(self.readthedocs_url, self.version, "instance_libraries", library_subdir)
                 doc.content(f"For this schema openMINDS provides a `library of instances <{library_link}>`_.")
                 doc.newline()
             if schema_name in ["License", "ContentType"]:
